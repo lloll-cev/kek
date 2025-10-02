5c4c6152dd7a75fe59bcf4beccb0670b9243c6b8
lloll-cev <Renatsariev135@gmail.com>
2025-10-02 12:55:30 +0300

    Add files via upload

---
+++ 1 (1).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, x, y;
+    
+    // Ввод значений переменных
+    cin >> a >> b >> x >> y;
+    
+    // Вывод пункта а)
+    cout << "a) 2 кг" << endl;
+    
+    // Вывод пункта б)
+    cout << "б) " << a << " 1" << endl;
+    
+    // Вывод пункта в)
+    cout << "в) " << x << " " << y << endl;
+    cout << "13 17" << endl;
+    cout << "19 " << b << endl;
+    cout << "5 " << y << endl;
+    
+    return 0;
+}
