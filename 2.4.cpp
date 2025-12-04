0b006b23e70feb5a2d9f4f219c7cebd1d54d990b
lloll-cev <Renatsariev135@gmail.com>
2025-12-04 20:31:16 +0300

    Add files via upload

---
+++ 2.4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() { double a;
+    cout << "Введите величину стороны квадрата a: ";
+    cin >> a;
+    double p = a*4;
+    cout << "Периметр квадрата равен: " << p << endl ;
+}
