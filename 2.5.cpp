a0a4fb5ffb309b511f6c8d31fae84a32879c64d9
lloll-cev <Renatsariev135@gmail.com>
2025-12-04 20:37:09 +0300

    Add files via upload

---
+++ 2.5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {double a;
+    cout << "Радиус окружности равен: ";
+    cin >> a;
+    double d = 2*a;
+    cout << "Диаметр равен: " << d << endl;
+
+}
